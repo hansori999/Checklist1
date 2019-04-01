f6978bc35f3b4dcffa029f053b54aba1b241a20b
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-04-01 15:27:31 +0900

    End of table View 1 ( 1 row of table table )

---
+++ Checklist1/ChecklistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Checklist1
+//
+//  Created by Myoung-Wan Koo on 01/04/2019.
+//  Copyright © 2019 Myoung-Wan Koo. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK:- Table View Data Source
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "ChecklistItem",for: indexPath)
+            
+            return cell
+    }
+
+
+}
+
